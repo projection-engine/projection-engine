e91d7fc7294a3f320ae0d1c4b7735187972a6322
Gustavo Roque <gustavomicaelbarbosa@gmail.com>
2022-02-14 22:08:21 -0300

    useHotKeys

--- src/views/editor/Editor.jsx
+++ src/views/editor/Editor.jsx
@@ -6,15 +6,18 @@ import ResizableBar from "../../components/resizable/ResizableBar";
 import SceneView from "../scene/SceneView";
 
 import getOptions from "../../pages/project/utils/getOptions";
-import {useContext, useEffect, useMemo, useRef} from "react";
+import {useContext, useEffect, useMemo, useRef, useState} from "react";
 import QuickAccessProvider from "../../services/hooks/QuickAccessProvider";
-import useControl from "../../services/hooks/useControl";
+import useHotKeys, {KEYS} from "../../services/hooks/useHotKeys";
 import PropTypes from "prop-types";
 import ControlProvider from "../../components/tabs/components/ControlProvider";
+import cloneClass from "../../pages/project/utils/misc/cloneClass";
+import randomID from "../../pages/project/utils/misc/randomID";
+import {ENTITY_ACTIONS} from "../../services/engine/ecs/utils/entityReducer";
 
 export default function Editor(props) {
     const quickAccess = useContext(QuickAccessProvider)
-    const fullscreenRef = useRef()
+
 
     const options = useMemo(() => {
 
@@ -35,7 +38,6 @@ export default function Editor(props) {
                 style={{fontSize: '1.2rem'}}
                 className={`material-icons-round`}>video_settings</span>,
             (newTab) => {
-                console.trace('HERE', newTab)
                 if (newTab === 0)
                     props.engine.setCanRender(true)
                 else
@@ -46,14 +48,89 @@ export default function Editor(props) {
         )
 
     }, [options])
+    const [toCopy, setToCopy] = useState()
+    useHotKeys({
+        disabled: controlProvider.tab !== 0,
+        actions: [
+            {
+                require: [KEYS.ControlLeft, KEYS.KeyS],
+                callback: () => {
+                    props.serializer.save()
+                }
+            },
+            {
+                require: [KEYS.ControlLeft, KEYS.KeyC],
+                callback: () => {
+                    setToCopy(props.engine.selectedElement)
+                    if (props.engine.selectedElement)
+                        props.setAlert({
+                            type: 'info',
+                            message: 'Entity copied.'
+                        })
+                }
+            },
+            {
+                require: [KEYS.ControlLeft, KEYS.ShiftLeft, KEYS.KeyF],
+                callback: () => {
+                    const el = document.getElementById('fullscreen-element-' + props.id)
+                    if(el) {
+                        if(!document.fullscreenElement)
+                        el.requestFullscreen()
+                        else
+                            document.exitFullscreen()
+                    }
+                }
+            },
+            {
+                require: [KEYS.ControlLeft, KEYS.ShiftLeft, KEYS.KeyH],
+                callback: () => props.settings.fpsVisibility = !props.settings.fpsVisibility
 
-    useControl(props.engine, props.serializer.save, props.settings, fullscreenRef)
+            },
+            {
+                require: [KEYS.Delete],
+                callback: () => {
+                    if (props.engine.selectedElement) {
+                        props.engine.dispatchEntities({type: ENTITY_ACTIONS.REMOVE, payload: {entityID: props.engine.selectedElement}})
+                        props.setAlert({
+                            type: 'success',
+                            message: 'Entity deleted.'
+                        })
+                    }
+                }
+            },
+            {
+                require: [KEYS.ControlLeft, KEYS.KeyV],
+                callback: () => {
+                    const found = props.engine.entities.find(e => e.id === toCopy)
+                    if (toCopy && found) {
+                        let clone = cloneClass(found)
+                        clone.id = randomID()
+
+                        clone.name += ' - clone'
+
+                        let newComponents = {}
+                        Object.keys(clone.components).forEach(c => {
+                            const cClone = cloneClass(clone.components[c])
+                            cClone.id = randomID()
+                            newComponents[c] = cClone
+                        })
+                        clone.components = newComponents
+                        props.engine.dispatchEntities({type: ENTITY_ACTIONS.ADD, payload: clone})
+                    } else
+                        props.setAlert({
+                            type: 'info',
+                            message: 'Nothing to paste.'
+                        })
+                }
+            }
+        ]
+    })
 
 
     return (
         <div className={styles.viewportWrapper}>
             <div
-                ref={fullscreenRef}
+                id={'fullscreen-element-' + props.id}
                 style={{
                     position: 'relative',
                     width: '100%',
@@ -64,6 +141,7 @@ export default function Editor(props) {
                     <ViewportOptions
                         engine={props.engine}
                         id={props.id}
+                        fullscreenID={'fullscreen-element-' + props.id}
                     />
                     :
                     null
