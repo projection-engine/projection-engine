9386f54d1be675cc7113126f383ea88282069152
Gustavo Roque <gustavomicaelbarbosa@gmail.com>
2022-05-09 22:18:45 -0300

    [HOT-FIX] - [File rename fix; Skybox save fix]

--- src/project/hooks/useSerializer.js
+++ src/project/hooks/useSerializer.js
@@ -5,7 +5,7 @@ import EVENTS from "../utils/EVENTS";
 import ProjectLoader from "../utils/workers/ProjectLoader";
 import LoaderProvider from "../../components/loader/LoaderProvider";
 
-const fs = window.require('fs')
+
 export default function useSerializer(engine, setAlert, settings, id, quickAccess, currentTab) {
 
     const load = useContext(LoaderProvider)
@@ -17,29 +17,28 @@ export default function useSerializer(engine, setAlert, settings, id, quickAcces
         let promise = []
 
         if (id) {
-            promise.push(new Promise((resolve) => {
+            promise.push(new Promise(async (resolve) => {
                 load.pushEvent(EVENTS.PROJECT_SAVE)
                 const canvas = engine.gpu.canvas
                 const preview = canvas.toDataURL()
-                fs.readFile(fileSystem.path + '\\.meta', (e, res) => {
-                    if (res) {
-                        const old = JSON.parse(res.toString())
-                        fileSystem
-                            .updateProject(
-                                {
-                                    ...old,
-                                    preview: currentTab === 0 ? preview : old.preview,
-                                    entities: engine.entities.length,
-                                    meshes: engine.meshes.length,
-                                    materials: engine.materials.length,
-                                    lastModification: (new Date()).toDateString(),
-                                    creation: settings.creationDate
-                                },
-                                settings)
-                            .then(() => resolve())
-                    } else
-                        resolve()
-                })
+                const res = await fileSystem.readFile(fileSystem.path + '\\.meta')
+                if (res) {
+                    const old = JSON.parse(res.toString())
+                    fileSystem
+                        .updateProject(
+                            {
+                                ...old,
+                                preview: currentTab === 0 ? preview : old.preview,
+                                entities: engine.entities.length,
+                                meshes: engine.meshes.length,
+                                materials: engine.materials.length,
+                                lastModification: (new Date()).toDateString(),
+                                creation: settings.creationDate
+                            },
+                            settings)
+                        .then(() => resolve())
+                } else
+                    resolve()
             }))
         }
 
@@ -56,24 +55,25 @@ export default function useSerializer(engine, setAlert, settings, id, quickAcces
 
                         await Promise.all(all.map(a => {
                             return new Promise(async (resolve1) => {
-                                if (a && a.data && a.type === 'entity' && !engine.entities.find(e => e.id === a.data.id)){
+                                if (a && a.data && a.type === 'entity' && !engine.entities.find(e => e.id === a.data.id)) {
                                     resolve1(await fileSystem.deleteFile(fileSystem.path + '\\logic\\' + a.data.id + '.entity', true))
-                                }
-                                else
+                                } else
                                     resolve1()
                             })
                         }))
 
-                        await Promise.all(engine.entities.map(e => {
-                            return new Promise((resolve) => {
-
-                                const str = JSON.stringify(structuredClone(e))
-
-                                fileSystem
-                                    .updateEntity(str, e.id)
-                                    .then(() => resolve())
-                            })
-                        }))
+                        try {
+                            await Promise.all(engine.entities.map(e => {
+                                return new Promise((resolve) => {
+                                    const str = JSON.stringify(e)
+                                    fileSystem
+                                        .updateEntity(str, e.id)
+                                        .then(() => resolve())
+                                })
+                            }))
+                        } catch (err) {
+                            console.log(err)
+                        }
                         setAlert({
                             type: 'success',
                             message: 'Project saved.'
