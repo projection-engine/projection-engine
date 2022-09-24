e2eeda03f175c72c99e94c5d8b5d3f65d5e91120
facobackup <gustavomicaelbarbosa@gmail.com>
2022-09-24 13:18:49 -0300

    - Fixed initial position of entity when creating it - Added option to center on origin (entity)

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ yarn package
 yarn electron-forge package --platform linux
 
 cd out/projection-engine-win32-x64/resources/app
-rm -r .idea
-rm -r src
-rm -r public/engine
-rm -r public/backend
+#rm -r .idea
+#rm -r src
+#rm -r public/engine
+#rm -r public/backend
 
 cd ../../../projection-engine-linux-x64/resources/app
-rm -r .idea
-rm -r src
-rm -r public/engine
-rm -r public/backend
-
+#rm -r .idea
+#rm -r src
+#rm -r public/engine
+#rm -r public/backend
+#
