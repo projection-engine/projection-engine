a673a101407b8b17986d6340be16331175e28791
facobackup <gustavomicaelbarbosa@gmail.com>
2022-12-21 15:31:55 -0300

    - Adding eslint - Removing unused dependencies and typings

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"overrides": [
+		{
+			files: ["*.svelte"],
+			processor: "svelte3/svelte3"
+		}
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint",
+		"svelte3"
+	],
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		]
+	}
+}
