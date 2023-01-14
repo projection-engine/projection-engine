70f0ee8dacfbffe228c0dfc1b831e3297c0be01a
facobackup <gustavomicaelbarbosa@gmail.com>
2023-01-14 12:16:06 -0300

    - Organizing uniform distribution on uber-shader - Adding new attributes to material - Adding state change to material inputs based on rendering mode

--- frontend/views/editor/views/shader-editor/templates/nodes/Material.ts
+++ frontend/views/editor/views/shader-editor/templates/nodes/Material.ts
@@ -2,6 +2,7 @@ import ShaderNode from "../ShaderNode"
 import DATA_TYPES from "../../../../../../../engine-core/static/DATA_TYPES"
 import NODE_TYPES from "../../libs/material-compiler/templates/NODE_TYPES"
 import checkGlslFloat from "../../utils/check-glsl-float";
+import MATERIAL_RENDERING_TYPES from "../../../../../../../engine-core/static/MATERIAL_RENDERING_TYPES";
 
 
 function arrayToGlsl(a) {
@@ -10,11 +11,7 @@ function arrayToGlsl(a) {
 }
 
 export default class Material extends ShaderNode {
-
     canBeDeleted = false
-
-    alphaTested = false
-    isSky = false
     doubleSided = false
     ssrEnabled = false
 
@@ -26,12 +23,22 @@ export default class Material extends ShaderNode {
     al = [0, 0, 0]
     flatShading = false
 
+    renderingType = MATERIAL_RENDERING_TYPES.ISOTROPIC
+    anisotropicRotation = 0
+    anisotropy = 0
+    clearCoat = 0
+    sheen = 0
+    sheenTint = 0
+
+    static #updateInput(key: String, value: any, instance: Material) {
+        instance.inputs.find(i => i.key === key).disabled = value
+    }
+
     constructor() {
         const allTypes = [DATA_TYPES.VEC4, DATA_TYPES.VEC3, DATA_TYPES.VEC2, DATA_TYPES.FLOAT, DATA_TYPES.INT]
         super([
             {label: "Albedo", key: "al", accept: allTypes, type: DATA_TYPES.COLOR},
             {label: "Normal", key: "normal", accept: allTypes},
-
             {label: "Ambient Occlusion", key: "ao", accept: allTypes},
             {label: "Roughness", key: "roughness", accept: allTypes, type: DATA_TYPES.FLOAT, max: 1, min: 0},
             {label: "Metallic", key: "metallic", accept: allTypes, type: DATA_TYPES.FLOAT, max: 1, min: 0},
@@ -45,18 +52,158 @@ export default class Material extends ShaderNode {
                 max: 1,
                 min: 0
             },
-            {label: "Refraction index", key: "refraction", accept: allTypes, type: DATA_TYPES.FLOAT, disabled:true},
-
             {label: "Screen-space reflections", key: "ssrEnabled", type: DATA_TYPES.CHECKBOX},
-
-
-            {label: "Is alpha tested", key: "alphaTested", type: DATA_TYPES.CHECKBOX},
-
             {label: "Double sided", key: "doubleSided", type: DATA_TYPES.CHECKBOX},
-            {label: "Is sky", key: "isSky", type: DATA_TYPES.CHECKBOX},
-            {label: "Flat-shading", key: "flatShading", type: DATA_TYPES.CHECKBOX}
+            {
+                label: "Rendering type",
+                key: "renderingType",
+                type: DATA_TYPES.OPTIONS,
+                options: [
+                    {label: "Isotropic", data: MATERIAL_RENDERING_TYPES.ISOTROPIC},
+                    {label: "Anisotropic", data: MATERIAL_RENDERING_TYPES.ANISOTROPIC},
+                    {label: "Sky", data: MATERIAL_RENDERING_TYPES.SKY},
+                    {label: "Transparency", data: MATERIAL_RENDERING_TYPES.TRANSPARENCY},
+                    {label: "Clear-Coat", data: MATERIAL_RENDERING_TYPES.CLEAR_COAT},
+                    {label: "Sheen", data: MATERIAL_RENDERING_TYPES.SHEEN},
+                    {label: "Unlit", data: MATERIAL_RENDERING_TYPES.UNLIT},
+                ]
+            },
+
+            {label: "Refraction index", key: "refraction", accept: allTypes, type: DATA_TYPES.FLOAT, disabled: true},
+
+            {label: "Anisotropic rotation", key: "anisotropicRotation", type: DATA_TYPES.FLOAT},
+            {label: "Anisotropy", key: "anisotropy", type: DATA_TYPES.FLOAT},
+
+            {label: "Clear coat", key: "clearCoat", type: DATA_TYPES.FLOAT},
+
+            {label: "Sheen", key: "sheen", type: DATA_TYPES.FLOAT},
+            {label: "Sheen tint", key: "sheenTint", type: DATA_TYPES.FLOAT},
         ], [])
 
+        const rT = this.inputs.find(i => i.key === "renderingType")
+        rT.onChange = (newValue: number) => {
+            switch (newValue) {
+                case MATERIAL_RENDERING_TYPES.ISOTROPIC:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", false, this)
+                    Material.#updateInput("ao", false, this)
+                    Material.#updateInput("roughness", false, this)
+                    Material.#updateInput("metallic", false, this)
+                    Material.#updateInput("emission", false, this)
+                    Material.#updateInput("opacity", true, this)
+                    Material.#updateInput("ssrEnabled", false, this)
+                    Material.#updateInput("doubleSided", false, this)
+                    Material.#updateInput("refraction", true, this)
+                    Material.#updateInput("anisotropicRotation", true, this)
+                    Material.#updateInput("anisotropy", true, this)
+                    Material.#updateInput("clearCoat", true, this)
+                    Material.#updateInput("sheen", true, this)
+                    Material.#updateInput("sheenTint", true, this)
+                    break
+                case MATERIAL_RENDERING_TYPES.ANISOTROPIC:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", false, this)
+                    Material.#updateInput("ao", false, this)
+                    Material.#updateInput("roughness", false, this)
+                    Material.#updateInput("metallic", false, this)
+                    Material.#updateInput("emission", false, this)
+                    Material.#updateInput("opacity", true, this)
+                    Material.#updateInput("ssrEnabled", false, this)
+                    Material.#updateInput("doubleSided", false, this)
+                    Material.#updateInput("refraction", true, this)
+                    Material.#updateInput("anisotropicRotation", false, this)
+                    Material.#updateInput("anisotropy", false, this)
+                    Material.#updateInput("clearCoat", true, this)
+                    Material.#updateInput("sheen", true, this)
+                    Material.#updateInput("sheenTint", true, this)
+                    break
+                case MATERIAL_RENDERING_TYPES.SKY:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", true, this)
+                    Material.#updateInput("ao", true, this)
+                    Material.#updateInput("roughness", true, this)
+                    Material.#updateInput("metallic", true, this)
+                    Material.#updateInput("emission", true, this)
+                    Material.#updateInput("opacity", true, this)
+                    Material.#updateInput("ssrEnabled", true, this)
+                    Material.#updateInput("doubleSided", true, this)
+                    Material.#updateInput("refraction", true, this)
+                    Material.#updateInput("anisotropicRotation", true, this)
+                    Material.#updateInput("anisotropy", true, this)
+                    Material.#updateInput("clearCoat", true, this)
+                    Material.#updateInput("sheen", true, this)
+                    Material.#updateInput("sheenTint", true, this)
+                    break
+                case MATERIAL_RENDERING_TYPES.TRANSPARENCY:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", false, this)
+                    Material.#updateInput("ao", true, this)
+                    Material.#updateInput("roughness", false, this)
+                    Material.#updateInput("metallic", true, this)
+                    Material.#updateInput("emission", false, this)
+                    Material.#updateInput("opacity", false, this)
+                    Material.#updateInput("ssrEnabled", false, this)
+                    Material.#updateInput("doubleSided", true, this)
+                    Material.#updateInput("refraction", false, this)
+                    Material.#updateInput("anisotropicRotation", true, this)
+                    Material.#updateInput("anisotropy", true, this)
+                    Material.#updateInput("clearCoat", true, this)
+                    Material.#updateInput("sheen", true, this)
+                    Material.#updateInput("sheenTint", true, this)
+                    break
+                case MATERIAL_RENDERING_TYPES.CLEAR_COAT:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", false, this)
+                    Material.#updateInput("ao", false, this)
+                    Material.#updateInput("roughness", false, this)
+                    Material.#updateInput("metallic", false, this)
+                    Material.#updateInput("emission", false, this)
+                    Material.#updateInput("opacity", true, this)
+                    Material.#updateInput("ssrEnabled", true, this)
+                    Material.#updateInput("doubleSided", true, this)
+                    Material.#updateInput("refraction", true, this)
+                    Material.#updateInput("anisotropicRotation", true, this)
+                    Material.#updateInput("anisotropy", true, this)
+                    Material.#updateInput("clearCoat", false, this)
+                    Material.#updateInput("sheen", true, this)
+                    Material.#updateInput("sheenTint", true, this)
+                    break
+                case MATERIAL_RENDERING_TYPES.SHEEN:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", false, this)
+                    Material.#updateInput("ao", false, this)
+                    Material.#updateInput("roughness", false, this)
+                    Material.#updateInput("metallic", false, this)
+                    Material.#updateInput("emission", false, this)
+                    Material.#updateInput("opacity", true, this)
+                    Material.#updateInput("ssrEnabled", false, this)
+                    Material.#updateInput("doubleSided", true, this)
+                    Material.#updateInput("refraction", true, this)
+                    Material.#updateInput("anisotropicRotation", true, this)
+                    Material.#updateInput("anisotropy", true, this)
+                    Material.#updateInput("clearCoat", true, this)
+                    Material.#updateInput("sheen", false, this)
+                    Material.#updateInput("sheenTint", false, this)
+                    break
+                case MATERIAL_RENDERING_TYPES.UNLIT:
+                    Material.#updateInput("al", false, this)
+                    Material.#updateInput("normal", true, this)
+                    Material.#updateInput("ao", true, this)
+                    Material.#updateInput("roughness", true, this)
+                    Material.#updateInput("metallic", true, this)
+                    Material.#updateInput("emission", true, this)
+                    Material.#updateInput("opacity", true, this)
+                    Material.#updateInput("ssrEnabled", true, this)
+                    Material.#updateInput("doubleSided", true, this)
+                    Material.#updateInput("refraction", true, this)
+                    Material.#updateInput("anisotropicRotation", true, this)
+                    Material.#updateInput("anisotropy", true, this)
+                    Material.#updateInput("clearCoat", true, this)
+                    Material.#updateInput("sheen", true, this)
+                    Material.#updateInput("sheenTint", true, this)
+                    break
+            }
+        }
         this.name = "Material"
     }
 
@@ -116,22 +263,52 @@ export default class Material extends ShaderNode {
                             name: checkGlslFloat(this.opacity),
                             type: DATA_TYPES.FLOAT
                         },
+                        emission = {
+                            name: arrayToGlsl(this.emission),
+                            type: DATA_TYPES.VEC3
+                        },
+
                         refraction = {
                             name: checkGlslFloat(this.refraction),
                             type: DATA_TYPES.FLOAT
                         },
-                        emission = {
-                            name: arrayToGlsl(this.emission),
-                            type: DATA_TYPES.VEC3
-                        }
+                        anisotropicRotation = {
+                            name: checkGlslFloat(this.anisotropicRotation),
+                            type: DATA_TYPES.FLOAT
+                        },
+                        anisotropy = {
+                            name: checkGlslFloat(this.anisotropy),
+                            type: DATA_TYPES.FLOAT
+                        },
+                        clearCoat = {
+                            name: checkGlslFloat(this.clearCoat),
+                            type: DATA_TYPES.FLOAT
+                        },
+
+                        sheen = {
+                            name: checkGlslFloat(this.sheen),
+                            type: DATA_TYPES.FLOAT
+                        },
+                        sheenTint = {
+                            name: checkGlslFloat(this.sheenTint),
+                            type: DATA_TYPES.FLOAT
+                        },
+
                     }) {
 
-        return `
+        return ` 
             naturalAO = ${this.getDataBehaviour(ao)};
             roughness = ${this.getDataBehaviour(roughness)};
             metallic = ${this.getDataBehaviour(metallic)};
             refractionIndex = ${this.getDataBehaviour(refraction)};
-            alpha = ${this.getDataBehaviour(opacity)};
+            
+            anisotropicRotation = ${this.getDataBehaviour(anisotropicRotation)};
+            anisotropy          = ${this.getDataBehaviour(anisotropy)};
+            clearCoat           = ${this.getDataBehaviour(clearCoat)};
+            sheen               = ${this.getDataBehaviour(sheen)};
+            sheenTint           = ${this.getDataBehaviour(sheenTint)};
+            
+            alpha = ${this.renderingType === MATERIAL_RENDERING_TYPES.TRANSPARENCY ? this.getDataBehaviour(opacity) : "1."};
             albedo = ${this.getData(al)};
             ${normal ? "computeTBN();" : ""}
             N = ${normal ? `normalize(TBN * ((${this.getData(normal)} * 2.0)- 1.0))` : "normalVec"};
