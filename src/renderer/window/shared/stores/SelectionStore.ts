c71615b558e3627ffc6a6f4770aab396e0b871d3
facobackup <gustavomicaelbarbosa@gmail.com>
2023-07-22 12:37:24 -0300

    [STD-341] create pull-request

--- src/renderer/window/shared/stores/SelectionStore.ts
+++ src/renderer/window/shared/stores/SelectionStore.ts
@@ -3,6 +3,7 @@ import SelectionTargets from "../../../../shared/enums/SelectionTargets"
 
 export default class SelectionStore extends AbstractStore{
 	constructor() {
+
 		super({TARGET: SelectionTargets.ENGINE, array: <string[]>[]})
 	}
 }
